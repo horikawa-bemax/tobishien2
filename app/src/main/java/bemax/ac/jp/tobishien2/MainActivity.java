8d1d41805ab080e81cb1b5ec4c51d4ccdbf2b67b
horikawa <horikawa@bemax.jp>
2016-11-10 17:06:09 +0900

    scrollViewがImageViewのタッチイベントを邪魔する問題を解決

--- app/src/main/java/bemax/ac/jp/tobishien2/MainActivity.java
+++ app/src/main/java/bemax/ac/jp/tobishien2/MainActivity.java
@@ -16,6 +16,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnTouchListener{
     TextView tv;
+    ScrollView sv;
     LinearLayout ll;
     ArrayList<ImageView> ivs;
 
@@ -31,37 +32,52 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         tv = (TextView)findViewById(R.id.textView);
         tv.setText(""+point.x+"*"+point.y);
 
+        sv = (ScrollView)findViewById(R.id.scrollView);
         ll = (LinearLayout) findViewById(R.id.thisLayout);
 
         ivs = new ArrayList<ImageView>();
         for(int i=0; i<10; i++){
             ImageView view = new ImageView(this);
             view.setImageResource(R.drawable.yatta);
+            LinearLayout.LayoutParams layout = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            view.setLayoutParams(layout);
             ivs.add(view);
             ll.addView(view);
             view.setOnTouchListener(this);
         }
 
-
+        //sv.setVerticalScrollbarPosition(5);
+        //sv.setOnTouchListener(this);
 
     }
 
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
         boolean res = false;
+        if(view instanceof ImageView) {
             switch (motionEvent.getAction()) {
                 case MotionEvent.ACTION_DOWN:
+                    sv.requestDisallowInterceptTouchEvent(true); // scrollViewのスクロールを無効化
+                    Log.d("event", "douw");
                     res = true;
                     break;
                 case MotionEvent.ACTION_MOVE:
-                    float newX = view.getX() + motionEvent.getX();
-                    tv.setText("" + newX);
+                    Log.d("event", "move");
+                    float newX = view.getX() + motionEvent.getX() - view.getWidth() / 2;
+                    tv.setText("");
                     view.setX(newX);
                     break;
                 case MotionEvent.ACTION_UP:
-                    view.setX(0);
+                    //view.setX(0);
+                    sv.requestDisallowInterceptTouchEvent(false); // scrollViewのスクロールを有効化
+                    Log.d("event", "up");
                     res = false;
+                    break;
+                default:
+                    Log.d("event", "default:" + motionEvent.getAction());
+                    res = true;
             }
+        }
         return res;//super.onTouchEvent(motionEvent);
     }
 }
